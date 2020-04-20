38d8dd5da6e815dcea918f25db02fb0b4c8ed5b5
duty-machine <63031384+duty-machine@users.noreply.github.com>
2020-04-20 15:30:20 -0300

    fix telegraph title

--- websites/telegraph.rb
+++ websites/telegraph.rb
@@ -9,6 +9,9 @@ register_website(
     author = document.css('header a[rel=author]').first.content
     content = document.css('article').first
 
+    content.css('h1').first.remove
+    content.css('address').first.remove
+
     {
       title: title,
       author: author,
